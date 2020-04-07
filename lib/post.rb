6879627aeaae5893cea87714534f3b41f60ff48b
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-07 02:36:31 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+    attr_accessor :title, :author
+    @@all = Array.new
+    
+   def initialize(title)
+    @title = title
+    @@all.push(self)
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if self.author 
+      self.author.name 
+    else
+      nil 
+    end
+  end
+  
+  
+  
+
+
+end
