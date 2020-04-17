7fdb479c3340e3daee6f5deb03f31101f2f42fdf
Evan Smith <professionalemail@zoho.com>
2020-04-17 04:40:15 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+  attr_accessor :title, :author
+ 
+  @@all = []
+ 
+  def initialize(title)
+    @title = title
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+
+ def author_name
+   self.author != nil ? self.author.name : nil
+  end
+ 
+  def self.all
+    @@all
+  end
+end
