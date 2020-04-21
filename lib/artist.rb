0fbe9d0f66247eaa639b414c2d90731166c39b0f
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-21 20:09:27 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,6 @@ class Artist
     @songs = []
   end
 
-  @@song_count = 0
-
   def songs
     Song.all.select { |song|  song.artist == self }
   end
@@ -15,18 +13,17 @@ class Artist
   def add_song(song_name)
     @songs << song_name
     song_name.artist = self
-    @@song_count += 1
+
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
     @songs << song
-    @@song_count += 1
   end
 
   def self.song_count
-    @@song_count
+    @songs.length
   end
 
 end
