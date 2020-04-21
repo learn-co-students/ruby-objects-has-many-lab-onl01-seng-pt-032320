421c809c4a08a1509ad0245c14c5af89848edb17
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-21 20:03:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,32 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
+  @@song_count = 0
+
   def songs
-    songs = []
-    songs < Song.new
+    Song.all.select { |song|  song.artist == self }
+  end
+
+  def add_song(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+    @songs << song
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song
-attr_accessor :name, :artist
+  attr_accessor :artist, :name
 
-def initialize(name)
-  @name = name
-end
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+
+    def self.all
+      @@all
+    end
 
 end
