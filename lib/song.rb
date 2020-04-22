45b62538530d2a0cf9c5255a59f3abe03ec2ee45
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-22 00:23:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Song
+  
+  attr_accessor :name, :artist
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def artist_name
+    self.artist.name
+    #binding.pry
+  end
+end
