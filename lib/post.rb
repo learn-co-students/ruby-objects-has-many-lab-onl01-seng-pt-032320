cf271c4c42d76fa4bc9bc5f25a70888beeda21b8
Bala Luguja <bluguja@gmail.com>
2020-04-26 02:41:06 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+ attr_accessor :title, :author
+  @@all = [] 
+  def initialize(title)
+    @title = title #shouldn't the variable be called @title?
+  @@all << self 
+  end
+ def self.all 
+     @@all 
+  end 
+def author_name 
+  if self.author
+    self.author.name
+  #else is not needed
+end 
+end 
+end
